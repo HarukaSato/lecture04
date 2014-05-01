d7ed098c145d7a4f4a8f7013118b60fcd9105b36
HarukaSato <sundy0315@gmail.com>
2014-05-01 12:40:31 +0900

    アニメーションの動作時間を速くした

--- js/slidein.js
+++ js/slidein.js
@@ -1,19 +1,27 @@
 var endSlideinFoxkeh = function(){
+//slideinFoxkehの最後のパーツ。functionで動作をするという指示。
 	var foxkeh = document.querySelector("#slidein-foxkeh img");
-
-var slideinFoxkehButton = document.querySelector("#slidein-foxkeh button");
-slideinFoxkehButton.addEventListener("mouseover", slideinFoxkeh);
-=======
+//foxkehという名前で"#slidein-foxkeh img"がくるように指定している。
 	foxkeh.setAttribute("class", "");
+//HTMLの属性を変更。foxkehに存在する”class”を、指定なし（？）
 };
 
 var startSlideinFoxkeh = function(){
+//slideinFoxkehの最初のパーツ。functionで動作をするという指示。
 	var foxkeh = document.querySelector("#slidein-foxkeh img");
+//foxkehという名前で"#slidein-foxkeh img"がくるように指定している
 	foxkeh.setAttribute("class", "slidein");
-
+//HTMLの属性を変更。foxkehに存在する”class”を、”slidein”にしている。
+//つまり、foxkehがスライドインされる。
 	foxkeh.addEventListener("animationend", endSlideinFoxkeh);
+//addEventListenerでイベントターゲットにイベントリスナーを 1 つ登録。
+//"animationend"にendSlideinFoxkehを登録する構文。
+//endSlideinFoxkehにうつるという指定を意味している。
 };
 
 var slideinFoxkehButton = document.querySelector("#slidein-foxkeh button");
+//slideinFoxkehButtonという名前で"#slidein-foxkeh button"がくるように指定している。
 slideinFoxkehButton.addEventListener("click", startSlideinFoxkeh);
->>>>>>> origin/make_slidein_retriable
+//addEventListenerでイベントターゲットにイベントリスナーを 1 つ登録。
+//"click"にstartSlideinFoxkehを登録する構文。
+//startSlideinFoxkehの動作にうつるという指定を意味している。
